294c91dbc5b81c561361914bd5ad890ef0be2f9e
Frieder Prinz <frieder@prinz.no>
2019-10-29 12:58:52 +0100

    merge and name changes

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const pg = require("pg");
-const dbURI = "postgres://qgmznzykbumzex:bdde0f1eda44c90c849c120f53b24c702aae8d782adb6f11b33bb535cb00d284@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d1kp021qnmfhaf" + "?ssl=true";
-const connstring = process.env.DATABASE_URL || dbURI;
-const pool = new pg.pool({ connectionString: connstring });
+const express = require("express");
+const cors = require("cors");//må installeres og brukes mens vi jobber med appen
+const app = express();
+
+app.use(cors());//allow all CORS requests
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("w/e");
+});
