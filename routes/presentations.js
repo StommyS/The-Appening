e9921a8f9b8616fb06b7a2c86ce3a6c36c44c8de
StommyS <56321130+StommyS@users.noreply.github.com>
2019-11-21 10:50:33 +0100

    add and see
    small presentation and updata info

---
+++ routes/presentations.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const route = express.Router();
+
+
+route.get("/:presentationID", function(req,res,next){
+
+});
+
+
+//base/applications/presentationID/slideID
+route.get("/:presentationID/:slideID", function(req,res,next){
+
+});
+
+
+route.put('/save', async function(req, res) {
+    let updata = req.body; //the data sent from the client
+
+    let finduser = undefined;//sql to fetch user
+    let user = undefined; // execute sql and get user
+    if (user) {
+        // find presentation
+        if(presentation) { // if already exists
+            // presentation = new presentation we got here
+            // push to database
+            //if database push ok send 200
+            //else send 500 database went wrong
+        }
+        //necessary??
+        else {
+            // push presentation as NEW
+        }
+    }
+    else {
+        res.status(400).json({message: "you don't exist"}).end();
+    }
+});
+
+//-------------End points presentations--------------------------------
+
+route.post('/', async function (req, res) {
+    let updata = req.body;
+
+    try {
+        let createdPresentation = await db.createpresentation(updata.title);
+        if(await createdPresentation) {
+            res.status(200).json({message: "Presentation created successfully"});
+        } else {
+            res.status(500).json({message: "Couldn't create presentation!"});
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+});
+
+route.delete('/', async function (req, res) {
+    let updata = req.body;
+    
+    try {
+        let deletedPresentation = await db.deletepresentation(updata.title);
+        if(await deletedPresentation) {
+            res.status(200).json({message: "Deleted presentation"});
+        } else {
+            res.status(500).json({message: "Couldn't delete presentation!"});
+        }
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({error: err});
+    }
+ });
+
+module.exports = route;
---
+++ routes/users.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const route = express.Router();
+
+//const secrets = require('../secret.js');
+//const dbURI = secrets.dbURI;
+//const dbConnection  = process.env.DATABASE_URL || dbURI;
+//const db = require("../modules/db")(dbConnection);
+
+const authenticate = require('../modules/auth.js');
+
+// endpoint - users POST -----------------------
+
+route.post('/create', async function (req, res) {
+    let updata = req.body;
+
+    const hash = crypto.createHmac('sha256', secret).update(updata.password).digest('hex');
+
+    try {
+        let createdUser = await db.createuser(updata.name, hash, updata.email);
+        if(await createdUser) {
+            res.status(200).json({message: "User created successfully", user: createdUser});
+        }
+        else {
+            res.status(500).json({message: "We couldn't create that user."});
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+});
+
+route.get('/', async function(req,res) {
+    let updata = req.body;
+    let user = null;
+    try {
+        user = await db.getuser(updata.name);
+        if(await user) {
+            await res.status(200).json({message: "User obtained", user: user});
+        }
+        else {
+            res.status(500).json({message: "We couldn't find that user."})
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({error: err});
+    }
+});
+
+route.delete('/delete', async function (req, res) {
+   let updata = req.body;
+   try {
+       let deletedUser = await db.deleteuser(updata.name);
+       await res.status(200).json({message: "User successfully deleted", user: deletedUser});
+   }
+   catch(err) {
+       console.log(err);
+       res.status(500).json({error: err});
+   }
+});
+
+route.delete('/wipe', async function(req,res) {
+    try {
+        let deletion = await db.deleteall();
+        if(await deletion) {
+            res.status(200).json({message: "Users table cleared."});
+        }
+        else {
+            res.status(500).json({message: "Could not clear users table."})
+        }
+    }
+    catch(err) {
+        console.log(err);
+        res.status(500).json({error: err});
+    }
+});
+
+route.post('/update', async function (req, res) {
+    let updata = req.body;
+
+    const hash = crypto.createHmac('sha256', secret).update(updata.password).digest('hex');
+
+    try {
+        let updatedUser = await db.updateuser(updata.name, hash, updata.email);
+        if(await updatedUser) {
+            res.status(200).json({message: "User updated successfully", user: updatedUser});
+        } else {
+            res.status(500).json({message: "Couldn't update user."});
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err });
+    }
+});
+
+
+//TEMP ARRAY
+const database = [];
+
+route.post('/login', async function (req, res) {
+    let reqinfo = req.body;
+    let user = database.find(user => user.name === reqinfo.name);
+    //let user = await db.getuser(reqinfo.name);
+
+    if(user) {
+        let pw = user.password;
+        if (pw === reqinfo.password) {
+            //create a token
+            res.status(200).json({message:"you're logged in", token:check});
+        }
+        else {
+            res.status(403).json({message:"not the right password"});
+        }
+    }
+    else{
+        res.status(400).json({message: "user does not exist"});
+    }
+    // test if user exists
+    // test if password is correct
+
+
+});
+
+
+
+module.exports = route;
