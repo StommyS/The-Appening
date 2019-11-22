551d56d7067547cf1bf4c890da08fb0651cfcb5f
StommyS <56321130+StommyS@users.noreply.github.com>
2019-11-22 08:16:55 +0100

    Some problems with server
    Cant get the server to actually connect to database, dont know why

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const pg = require("pg");
-const dbURI = "postgres://qgmznzykbumzex:bdde0f1eda44c90c849c120f53b24c702aae8d782adb6f11b33bb535cb00d284@ec2-54-217-225-16.eu-west-1.compute.amazonaws.com:5432/d1kp021qnmfhaf" + "?ssl=true";
-const connstring  = process.env.DATABASE_URL || dbURI;
-const pool = new pg.Pool({ connectionString: connstring });
-
 const cors = require('cors');
 const express = require('express');
-const app = express(); //server-app
+const app = express();
 
-// middleware ------------------------------------
-app.use(cors()); //allow all CORS requests
-app.use(express.json()); //for extracting json in the request-body
-app.use('/', express.static('public'));//for serving client files
+app.use(cors());
+app.use(express.json());
 
-// start server -----------------------------------
-var port = process.env.PORT || 3000;
+const userRoutes = require("./routes/users.js");
+app.use("/users", userRoutes);
+
+const presentationRoutes = require("./routes/presentations.js")
+//const loginRoutes = require("./routes/users.js")
+//app.use("/users", loginRoutes); // uses the code in routes/users
+app.use("/presentation", presentationRoutes)
+
+let port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Server listening on port 3000!');
 });
