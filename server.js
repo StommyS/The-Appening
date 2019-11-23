019abb8c819379fe9ae54718648d4c51afd02242
Frieder Prinz <frieder@prinz.no>
2019-11-23 13:44:19 +0100

    fixes of stuff that apparently broke

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use('/', express.static('public'));
 const userRoutes = require("./routes/users.js");
 app.use("/users", userRoutes);
 
-const presentationRoutes = require("./routes/presentations.js")
-app.use("/presentations", presentationRoutes)
+const presentationRoutes = require("./routes/presentations.js");
+app.use("/presentations", presentationRoutes);
 
 let port = process.env.PORT || 3000;
 app.listen(port, function () {
